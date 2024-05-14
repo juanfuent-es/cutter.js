98b7d797e39f19e95dd5259f5d3d5c3fe3380548
JuanFuent.es <input@juanfuent.es>
2024-05-13 22:39:07 -0600

    Update Offsets

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const PX_RATIO = window.devicePixelRatio
+/*
+ * Re-maps a number from one range to another. Thank's processing
+ * @see: https://processing.org/reference/map_.html
+ */
+Math.map = (n, start, stop, start2, stop2) => {
+    const newval = (n - start) / (stop - start) * (stop2 - start2) + start2
+    return newval
+}
+
 import HTMLGeometry from "./geometry"
 import {
     gsap
@@ -6,6 +16,11 @@ import {
 class Cutter {
     constructor(_svg) {
         this.container = _svg
+        this.offset = {
+            x: 0,
+            y: 0
+        }
+        //
         this.drag = false
         this.drag = new HTMLGeometry(this.container.querySelector(".drag"))
         this.clip = new HTMLGeometry(this.container.querySelector(".clip"))
@@ -37,16 +52,18 @@ class Cutter {
         this.drag.dom_element.addEventListener('touchstart', e => this.onDragDown())
         document.addEventListener('touchmove', e => this.onDragMove())
         document.addEventListener('touchend', e => this.onDragUp())
+        //
     }
     //events
     onDragDown(e) {
         this.dragging = true
         gsap.to(this.drag.dom_element, {
-            duration: .25,
+            transformOrigin: "center center",
+            duration: .15,
             x: 250,
             y: 250,
-            scaleX: .35,
-            scaleY: .35,
+            scaleX: .65,
+            scaleY: .65,
             opacity: 0
         })
     }
@@ -65,6 +82,7 @@ class Cutter {
     onDragUp(e) {
         this.dragging = false
         gsap.to(this.drag.dom_element, {
+            transformOrigin: "center center",
             duration: .15,
             x: 250,
             y: 250,
@@ -75,10 +93,36 @@ class Cutter {
     }
 
     dragTo(_x, _y) {
-        this.img.dom_element.setAttribute("x", _x)
-        this.img.dom_element.setAttribute("y", _y)
+        this.offset = this.updateOffsets(_x, _y)
+        console.clear()
+        console.log("offset", this.offset)
+        // this.cutter(this.offset)
     }
 
+    cutter(_offset) {
+        let max_offset = Math.abs((this.img.dom_element.getAttribute("width") - this.clip.dom_element.getAttribute("width")) / 2)
+        let cut_x = Math.map(_offset.x, 0, 100, max_offset, -max_offset)
+        console.log(max_offset, cut_x)
+        //
+        this.img.dom_element.setAttribute("x", cut_x)
+        // this.img.dom_element.setAttribute("y", _offset.y)
+    }
+
+    updateOffsets(_x = 0, _y = 0) {
+        let _width = this.clip.dom_element.getAttribute("width")
+        let x = (~~(_x * 100) / _width)
+        x = Math.max(x, 0)
+        x = Math.min(x, 100)
+        //
+        let _height = this.clip.dom_element.getAttribute("height")
+        let y = (~~(_y * 100) / _height)
+        y = Math.max(y, 0)
+        y = Math.min(y, 100)
+        return {
+            x: ~~x,
+            y: ~~y
+        }
+    }
 }
 const svg = document.querySelector("#app")
 const move = new Cutter(svg)
